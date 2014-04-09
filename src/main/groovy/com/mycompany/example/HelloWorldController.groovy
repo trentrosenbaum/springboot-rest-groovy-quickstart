9b17aaa7b987c659585297ca1eb58da6703e2ba4
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2014-04-09 22:26:09 +0100

    Updated the HelloWorldController to be annotated with RestController.  This will assume the ResponseBody annotation is implied on RequestMapping annotated methods.

--- src/main/groovy/com/mycompany/example/HelloWorldController.groovy
+++ src/main/groovy/com/mycompany/example/HelloWorldController.groovy
@@ -1,21 +1,20 @@
-package com.mycompany.example;
+package com.mycompany.example
+
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/helloworld")
-    public @ResponseBody
-    HelloWorld greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public HelloWorld greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
 
         return new HelloWorld(counter.incrementAndGet(), String.format(template, name));
     }
